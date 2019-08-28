67e484ad352e35dafd624f25a337514b88a83b7c
joseph <1396871335@qq.com>
2019-08-28 18:10:10 +0800

    接口识别图形验证码

--- Case/demo04.py
+++ Case/demo04.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from PIL import Image, ImageEnhance
-import pytesseract  #注意需要配置pytesseract.py文件下的tesseract_cmd = '你的tesseract的路径'
+import pytesseract  # 注意需要配置pytesseract.py文件下的tesseract_cmd = '你的tesseract的路径'
 
 driver = webdriver.Chrome()
 driver.maximize_window()
@@ -15,7 +15,9 @@ print(EC.title_contains("注册"))
 driver.save_screenshot("/Users/yangchao/Desktop/乐学/img.png") # 保存网页截图
 # code_element = driver.find_element_by_xpath("//*[@id='getcode_num']")    # 定位验证码
 code_element = driver.find_element_by_id("getcode_num")
-print(code_element.location)     # 获取验证码的图片大小
+
+# print(code_element.location)     # 获取验证码的图片大小
+
 left = code_element.location['x']*2    # 横坐标
 top = code_element.location['y']*2     # 竖坐标
 right = code_element.size['width']*2+left
@@ -23,14 +25,15 @@ height = code_element.size["height"]*2+top
 
 img = Image.open("/Users/yangchao/Desktop/乐学/img.png").crop((left, top, right, height))
 img.save("/Users/yangchao/Desktop/乐学/img副本.png")
-image = Image.open("/Users/yangchao/Desktop/乐学/img副本.png")
-img2 = image.convert('L')
-sharpness = ImageEnhance.Contrast(img2)
-img3 = sharpness.enhance(3.0)
-img3.save("/Users/yangchao/Desktop/乐学/img副本-1.png")
-i4 = Image.open("/Users/yangchao/Desktop/乐学/img副本-1.png")
-text = pytesseract.image_to_string(i4)
-print(text)
+
+# image = Image.open("/Users/yangchao/Desktop/乐学/img副本.png")
+# img2 = image.convert('L')
+# sharpness = ImageEnhance.Contrast(img2)
+# img3 = sharpness.enhance(3.0)
+# img3.save("/Users/yangchao/Desktop/乐学/img副本-1.png")
+# i4 = Image.open("/Users/yangchao/Desktop/乐学/img副本-1.png")
+# text = pytesseract.image_to_string(i4)
+# print(text)
 
 
 driver.find_element_by_id("register_email").send_keys("13788@163.com")
@@ -42,7 +45,7 @@ user_element_class.send_keys("joseph")
 # 找到密码
 driver.find_element_by_name("password").send_keys("ych19951005")
 # 找到验证码
-driver.find_element_by_xpath("//*[@id='captcha_code']").send_keys(text)
+driver.find_element_by_xpath("//*[@id='captcha_code']").send_keys()
 # 点击注册
 driver.find_element_by_id("register-btn").click()
 time.sleep(10)
