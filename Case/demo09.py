ef0cf8ca1f99e7c0e5c7e77146e9650ae049a411
joseph <1396871335@qq.com>
2019-08-29 18:18:10 +0800

    配置文件

---
+++ Case/demo09.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.find_element_by_id()
---
+++ Case/read_ini.py
@@ -0,0 +1,29 @@
+# coding = utf-8
+import configparser
+
+class ReadIni(object):
+    def __init__(self, file_name=None, node=None):
+        if file_name == None:
+            self.file_name = "/Users/yangchao/PycharmProjects/project/config/local_element.ini"
+        if node == None:
+            self.node = "RegisterElement"
+        else:
+            self.node = node
+        self.cf = self.load_ini(self.file_name)
+
+# 加载文件
+    def load_ini(self,file_name):
+        cf = configparser.ConfigParser()
+        cf.read(file_name)
+        return cf
+
+# 获取value值
+    def get_value(self,key):
+        date = self.cf.get(self.node, key)
+        return date
+
+if __name__ == '__main__':
+    red_init = ReadIni()
+    print(red_init.get_value("user_name"))
+
+
