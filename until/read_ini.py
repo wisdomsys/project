fea09f92916319dec0b2a3ced2847946b22a100e
joseph <1396871335@qq.com>
2019-08-30 14:55:18 +0800

    111

--- until/read_ini.py
+++ until/read_ini.py
@@ -4,23 +4,23 @@ import configparser
 class ReadIni(object):
     def __init__(self, file_name=None, node=None):
         if file_name == None:
-            self.file_name = "/Users/joseph/PycharmProjects/project/config/local_element.ini"
+            file_name = "/Users/joseph/PycharmProjects/project/config/local_element.ini"
         if node == None:
             self.node = "RegisterElement"
         else:
             self.node = node
-        self.cf = self.load_ini(self.file_name)
+        self.cf = self.load_ini(file_name)
 
 # 加载文件
-    def load_ini(self,file_name):
+    def load_ini(self, file_name):
         cf = configparser.ConfigParser()
         cf.read(file_name)
         return cf
 
 # 获取value值
     def get_value(self,key):
-        date = self.cf.get(self.node, key)
-        return date
+        data = self.cf.get(self.node, key)
+        return data
 
 if __name__ == '__main__':
     red_init = ReadIni()
